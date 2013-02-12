b1338eaabf346266a71b8c1cf872c8e708a3929a
Yarik85 <yatsarevsky@gmail.com>
2013-02-12 14:14:00 +0200

    ticket:5 fixed all test OK

--- yaproject/urls.py
+++ yaproject/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
-import os
 from django.views.generic.simple import redirect_to
+import os
 
 from .vcard import views
 
@@ -15,11 +15,10 @@ urlpatterns = patterns('',
     url(r'^request_store/$', views.requests_store, name='requests'),
     url(r'^edit/$', views.edit_page, name='edit_page'),
     url(r'^sign-up/member/$', views.accounts_registration,
-        name='accounts-registration'),
+        name='sign-up'),
     url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'accounts/signup_member.html'}, name='login'),
-    url(r'^logout/$', views.logout_account,
-        name='logout-account'),
+        {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', views.logout_account, name='logout'),
     url(r'^accounts/profile/$', redirect_to, {'url': '/edit/'}),
 )
 
