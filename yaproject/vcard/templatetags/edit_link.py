db4cabc93720b2e6754662136e332c49ff7dc302
Yarik85 <yatsarevsky@gmail.com>
2013-02-14 11:43:51 +0200

    nothing

--- yaproject/vcard/templatetags/edit_link.py
+++ yaproject/vcard/templatetags/edit_link.py
@@ -1,12 +1,28 @@
 from django import template
 from django.core.urlresolvers import reverse
-from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
 
-@register.filter
-def admin_link(vcard, user):
-    if user.is_authenticated() and user.is_superuser:
-        link = reverse('admin:vcard_vcard_change', args=[vcard.pk])
-        return mark_safe('<a href="%s"> (admin) </a>' % link)
+class EditLinkNode(template.Node):
+    def __init__(self, target):
+        self.target = template.Variable(target)
+
+    def render(self, context):
+        obj = self.target.resolve(context)
+        pattern = 'admin:{0}_{1}_change'.format(obj._meta.app_label,
+            obj._meta.module_name)
+        return '<a href="{}">Edit</a>'.format(reverse(pattern,
+        args=[obj.pk]))
+
+
+def get_edit_link(parser, token):
+    try:
+        tag_name, target = token.split_contents()
+    except ValueError:
+        err = "%r tag requires only one arguments" % token.contents.split()[0]
+        raise template.TemplateSyntaxError(err)
+    return EditLinkNode(target)
+
+register.tag('get_edit_link', get_edit_link)
